a8101753db2349cf8ce6d114c975b4ba4b9fac7c
Camilla Rees <camilla.c.rees@gmail.com>
2023-06-01 14:44:40 -0700

    fixed image urls

--- js/game.js
+++ js/game.js
@@ -1,16 +1,16 @@
 'use strict';
 
 let imgUrlArr =
-['../assets/page1.jpg',
-  '../assets/page2.jpg',
-  '../assets/page3.jpg',
-  '../assets/page4.jpg',
-  '../assets/page5.jpg',
-  '../assets/page6.jpg',
-  '../assets/page7.jpg',
-  '../assets/page8.jpg',
-  '../assets/page9.jpg',
-  '../assets/page10.jpg'
+[ 'assets/page1.jpg',
+  'assets/page2.jpg',
+  'assets/page3.jpg',
+  'assets/page4.jpg',
+  'assets/page5.jpg',
+  'assets/page6.jpg',
+  'assets/page7.jpg',
+  'assets/page8.jpg',
+  'assets/page9.jpg',
+  'assets/page10.jpg'
 ];
 
 let storyArr =
